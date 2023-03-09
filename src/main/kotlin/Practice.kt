572b8a6631dd0a2624db4620b836a8239a2837c9
VaibhavRanga <vaibhavranga2@gmail.com>
2023-03-09 21:08:18 +0530

    Exercise

--- src/main/kotlin/Practice.kt
+++ src/main/kotlin/Practice.kt
@@ -1,21 +1,2 @@
-import kotlin.math.pow
-
 class Practice {
-}
-
-fun main() {
-    var tempNumber = 153
-    var count = 0
-    var result = 0
-    while (tempNumber > 0) {
-        tempNumber /= 10
-        println(tempNumber)
-        count++
-    }
-    tempNumber = 153
-    while (tempNumber > 0) {
-        val remainder = tempNumber % 10
-        result += (remainder.toDouble().pow(count)).toInt()
-        tempNumber /= 10
-    }
 }
