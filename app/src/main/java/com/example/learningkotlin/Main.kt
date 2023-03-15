67a16bb78981a763ec1b0862babee2edec8d52e3
VaibhavRanga <vaibhavranga2@gmail.com>
2023-03-15 15:24:09 +0530

    Modified practice file

--- app/src/main/java/com/example/learningkotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-package com.example.learningkotlin
-
-fun main() {
-
-    val languages = arrayOf("Kotlin", "Java", "C", "C++", "C#", "JavaScript", "Python")
-    println("Some of the top programming languages around the world are: ${languages.contentToString()}")
-}
