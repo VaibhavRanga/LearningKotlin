b42eed7868df161719c9c92522795b0d57d731cd
VaibhavRanga <vaibhavranga2@gmail.com>
2023-04-12 03:27:06 +0530

    Constraint Layout

--- app/src/main/java/com/example/learningkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learningkotlin/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.learningkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
